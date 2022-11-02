ad6d2d7d8e7d940354445f27f738b3fbd28dc892
Valtteri Laitinen <dev@valtlai.fi>
2022-11-02 23:17:11 +0200

    Update std

--- src/deps.js
+++ src/deps.js
@@ -1,4 +1,4 @@
-export * as path from "https://deno.land/std@0.97.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.161.0/path/mod.ts";
 export { default as EventEmitter } from "https://deno.land/x/events@v1.0.0/mod.ts";
 
 export function existsSync(p) {
